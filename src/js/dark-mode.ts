89e028533538ced370184a2cf3e85442585d0fbe
imarawn <142156357+imarawn@users.noreply.github.com>
2025-01-28 09:06:21 +0100

    migrate to ts?

--- src/js/dark-mode.ts
+++ src/js/dark-mode.ts
@@ -1,7 +1,7 @@
-const darkmodetoggleButtons = document.getElementsByClassName('darkmode-button');
+const darkModeToggleButtons = document.getElementsByClassName('darkmode-button') as HTMLCollectionOf<HTMLButtonElement>;
 
 // Loop through all the buttons and attach the event listener
-Array.from(darkmodetoggleButtons).forEach((button) => {
+Array.from(darkModeToggleButtons).forEach((button) => {
     button.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
         const mode = document.body.classList.contains('dark-mode') ? 'enabled' : 'disabled';
