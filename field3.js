0aa1b2164860100209ed49646fb727bb441f977a
soeren <weabung@gmx.ch>
2025-07-19 21:32:35 +0200

    emote description

--- field3.js
+++ field3.js
@@ -1,16 +1,21 @@
-export function showGifDetails(url, slug) {
+import {supabase} from "./supabaseclient.js";
+
+
+export async function showGifDetails(url, slug) {
     const field3 = document.querySelector('main .grid > div:last-child');
     field3.innerHTML = '';
 
     const encoded = btoa(encodeURIComponent(JSON.stringify({ url, slug })));
     const basePath = window.location.pathname.replace(/\/[^\/]*$/, '/');
     const shareUrl = `${basePath}share.html?d=${encoded}`;
+    const { data, error } = await supabase.rpc('get_emote_description', { given_slug: slug });
+    const description = data?.[0]?.description || 'No description available.';
 
     field3.innerHTML = `
     <div class="flex flex-col items-center text-center space-y-4">
       <img src="${url}" alt="preview" class="w-full max-h-full rounded shadow-lg" />
       <div class="text-white text-sm font-semibold">${slug}</div>
-      <div class="text-zinc-400 text-xs italic">More info coming soon...</div>
+      <div class="text-zinc-400 text-s italic">${description}</div>
       <button id="share-btn" class="mt-2 px-4 py-2 bg-brand text-black rounded hover:brightness-110 transition">
         🔗 Share
       </button>
